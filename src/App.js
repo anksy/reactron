5640fc414e05c128a5b3a8641dea3bec95fabd5a
Bhupendra Singh <bbhupendra007@gmail.com>
2020-03-18 12:57:45 +0530

    small electron react poc

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  state = {
+    countries: [],
+    chosen: undefined
+  }
+  
+  componentDidMount(){
+    fetch("https://restcountries.eu/rest/v2/all")
+    .then(response => response.json())
+    .then(countries => this.setState({ countries }))
+    .catch(error => console.log("error =>", error))
+  }
+
+  render(){
+    const { countries, chosen } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+           Select Country <select onChange={(e) => this.setState({ chosen: e.target.value})}>
+             {countries.map(country => <option value={country.name} key={country.name}>{country.name}</option>)}
+           </select>
+
+    {chosen && <i>You have chosen {chosen}</i>}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
